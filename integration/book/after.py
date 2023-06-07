1189863b7c6b97e5c90126f52541c235c432fb0b
a <a>
2023-06-07 18:51:02 +0900

    wip

--- integration/book/after.py
+++ integration/book/after.py
@@ -25,18 +25,21 @@ print("[INFO] cur-dir:", root_path)
 
 # mdbook のビルドを行う。
 os.chdir(book_path)
+print("[INFO] mdbook building")
 subprocess.run(["mdbook", "build"])
+print("[INFO] mdbook build completed")
 
 # 各 component のビルド成果物を取得する。
 ## カレントディレクトリを book/component にする
 os.chdir(comp_path)
 
 ## book/component/src/bin にある名前を全て得て、確認する。
+print("[INFO] trunk building")
 target_dir = os.path.join(comp_path, "src/bin/*.rs")
 files = glob.glob(target_dir)
-print("[INFO] files list:", target_dir)
+print("[INFO] files list:")
 for file_name in files:
-    print("    ", file_name)
+    print("-", file_name)
 
 ## book/component/index.html を上で得た各ファイルごとに書き換え、trunkでビルドする。
 index_html_name = os.path.join(comp_path, "index.html")
@@ -64,8 +67,10 @@ for file_name in files:
             continue
         print("[INFO] copying file:", file_name)
         shutil.copy(file_name, out_book_path)
+print("[INFO] trunk build completed")
 
 # 得られたhtmlファイルに対して、wasm や js のロードを行うための変更を与える。
+print("[INFO] html processing")
 ## ファイルの中身の文字列を得るためだけの関数
 ### // 変数のスコープやファイルをしっかり閉じているかどうかがpythonだとわかりにくいのでこれだけ別にした。
 def file_str(file_name):
@@ -106,36 +111,37 @@ os.chdir(out_book_path)
 
 for md_name in md_names:
     file_name = os.path.join(out_book_path, md_name + ".html")
-    content_html = file_str(file_name)
+    print("[INFO] file:", file_name)
 
+    content_html = file_str(file_name)
     # html の中にある <component> をすべて取得する。
     list_component = []
     for match_obj in re_component.finditer(content_html):
         list_component.append(match_obj.group(1))
-    print("[INFO] file:", file_name)
     print("[INFO] component:", list_component)
 
     # html にある <component> を全て div に直したhtmlをえる。
     processed_html = re_component.sub(r'<div id="\1"></div>', content_html)
 
     # 追記を行う文字列を精製する。
-    head_load = ""
-    body_load = ""
+    head_load_html = ""
+    body_load_html = ""
     for name in list_component:
-        head_load = head_load + load_head_html(name)
-        body_load = body_load + load_body_html(name)
+        head_load_html = head_load_html + load_head_html(name)
+        body_load_html = body_load_html + load_body_html(name)
 
     # 得られたhtmlをhtmlの構造に合わせて正規表現で分解する。
     match_obj = re_html.search(processed_html)
 
     # 分解して得られた文字列をhead_loadやbody_loadを加えた文字列にする。
-    body_load = '<script type="module">import init from \'/' + name + '.js\';init(\'/' + name + '_bg.wasm\');</script>'
-    head_html = "<head>" + match_obj.group(2) + head_load + "</head>"
-    body_html = "<body>" + match_obj.group(4) + body_load + "</body>"
+    head_html = "<head>" + match_obj.group(2) + head_load_html + "</head>"
+    body_html = "<body>" + match_obj.group(4) + body_load_html + "</body>"
     result = match_obj.group(1) + head_html + match_obj.group(3) + body_html + match_obj.group(5)
 
     # 得られた文字列をファイルに書き込む。
     with open(file_name, mode='w') as file:
         file.write(result)
+
+print("[INFO] process completed")
 
 sys.exit(0)
