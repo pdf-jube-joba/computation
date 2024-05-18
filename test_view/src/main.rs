9f8f19864b567049da0784402acd6a0d49ee368c
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-05-18 16:40:47 +0900

    OK

--- test_view/src/main.rs
+++ test_view/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let document = gloo::utils::document();
+    let element = document
+        .get_element_by_id("m")
+        .unwrap();
+    let examples = logic_circuit::example::examples();
+    let lc = examples.get(&"DFF".into()).unwrap().clone();
+    logic_circuit_view::set_machine(element, lc);
 }
