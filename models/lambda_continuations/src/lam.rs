2f4b2c797c34dcee7933ba797810e9c83a424028
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2024-08-29 11:55:11 +0900

    OK

--- models/lambda_continuations/src/lam.rs
+++ models/lambda_continuations/src/lam.rs
@@ -366,7 +366,7 @@ mod ext {
         Pred(Box<Lam>),
         IfZ(Box<Lam>, Box<Lam>, Box<Lam>),
         Let(Var, Box<Lam>, Box<Lam>),
-        Fix(Var, Box<Lam>, Box<Lam>),
+        Rec(Var, Box<Lam>, Box<Lam>),
     }
 
     impl Display for Lam {
@@ -380,7 +380,7 @@ mod ext {
                 Lam::Pred(e) => format!("pred {e}"),
                 Lam::IfZ(e1, e2, e3) => format!("if {e1} then {e2} else {e3}"),
                 Lam::Let(x, e1, e2) => format!("let {x} = {e1} in {e2}"),
-                Lam::Fix(x, e1, e2) => format!("fix {x} = {e1} in {e2}"),
+                Lam::Rec(x, e1, e2) => format!("fix {x} = {e1} in {e2}"),
             };
             write!(f, "{string}")
         }
@@ -389,8 +389,23 @@ mod ext {
     #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
     pub enum Value {
         Num(Number),
-        Bool(Bool),
         Function(Var, Lam),
+    }
+
+    fn num_to_exp(e: Number) -> Lam {
+        if e.is_zero() {
+            Lam::Zero
+        } else {
+            Lam::Succ(Box::new(num_to_exp(e - 1)))
+        }
+    }
+
+    fn exp_to_num(e: &Lam) -> Option<Number> {
+        match e {
+            Lam::Zero => Some(0.into()),
+            Lam::Succ(e) => Some(exp_to_num(&e)? + 1),
+            _ => None,
+        }
     }
 
     #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -405,9 +420,13 @@ mod ext {
             value_of_var: Value,
             exp: Lam,
         },
-        Fix {
+        Rec {
             var: Var,
             fix_exp: Lam,
+            exp: Lam,
+        },
+        Pred {
+            n: Num,
         },
     }
 
@@ -445,7 +464,7 @@ mod ext {
                     set.extend(e2.free_variables());
                     set.remove(x);
                 }
-                Lam::Fix(x, e1, e2) => {
+                Lam::Rec(x, e1, e2) => {
                     set.extend(e1.free_variables());
                     set.extend(e2.free_variables());
                     set.remove(x);
@@ -482,13 +501,147 @@ mod ext {
                     set.extend(e2.bound_variables());
                     set.insert(x.clone());
                 }
-                Lam::Fix(x, e1, e2) => {
+                Lam::Rec(x, e1, e2) => {
                     set.extend(e1.bound_variables());
                     set.extend(e2.bound_variables());
                     set.insert(x.clone());
                 }
             }
             set
+        }
+        fn alpha_conversion_canonical(self, vs: HashSet<Var>) -> Self {
+            fn alpha_conversion_canonical_rec(e: Lam, mut vs: variable::VarMap) -> Lam {
+                match e {
+                    Lam::Var(x) => Lam::Var(vs.get_table(&x)),
+                    Lam::Lam(x, e) => {
+                        vs.push_var(&x);
+                        let new_x = vs.get_table(&x);
+                        Lam::Lam(new_x, Box::new(alpha_conversion_canonical_rec(*e, vs)))
+                    }
+                    Lam::App(e1, e2) => Lam::App(
+                        Box::new(alpha_conversion_canonical_rec(*e1, vs.clone())),
+                        Box::new(alpha_conversion_canonical_rec(*e2, vs)),
+                    ),
+                    Lam::Zero => Lam::Zero,
+                    Lam::Succ(e) => Lam::Succ(Box::new(alpha_conversion_canonical_rec(*e, vs))),
+                    Lam::Pred(e) => Lam::Pred(Box::new(alpha_conversion_canonical_rec(*e, vs))),
+                    Lam::IfZ(e1, e2, e3) => Lam::IfZ(
+                        Box::new(alpha_conversion_canonical_rec(*e1, vs.clone())),
+                        Box::new(alpha_conversion_canonical_rec(*e2, vs.clone())),
+                        Box::new(alpha_conversion_canonical_rec(*e3, vs)),
+                    ),
+                    Lam::Let(x, e1, e2) => {
+                        let new_e1 = alpha_conversion_canonical_rec(*e1, vs.clone());
+                        let new_x = vs.get_table(&x);
+                        let new_e2 = alpha_conversion_canonical_rec(*e2, vs);
+                        Lam::Let(new_x, Box::new(new_e1), Box::new(new_e2))
+                    }
+                    Lam::Rec(x, e1, e2) => {
+                        let new_e1 = alpha_conversion_canonical_rec(*e1, vs.clone());
+                        let new_x = vs.get_table(&x);
+                        let new_e2 = alpha_conversion_canonical_rec(*e2, vs);
+                        Lam::Rec(new_x, Box::new(new_e1), Box::new(new_e2))
+                    }
+                }
+            }
+            let vs = variable::VarMap::new_iter(self.free_variables().into_iter().chain(vs));
+            alpha_conversion_canonical_rec(self, vs)
+        }
+
+        fn is_value(&self) -> Option<Self::Value> {
+            if let Some(n) = exp_to_num(&self) {
+                Some(Value::Num(n))
+            } else if let Lam::Lam(x, e) = &self {
+                Some(Value::Function(x.clone(), e.as_ref().clone()))
+            } else {
+                None
+            }
+        }
+
+        fn value_as_exp(v: Self::Value) -> Self {
+            match v {
+                Value::Num(n) => num_to_exp(n),
+                Value::Function(x, e) => Lam::Lam(x, Box::new(e)),
+            }
+        }
+
+        fn is_redex(&self) -> Option<Self::RedexInfo> {
+            match self {
+                Lam::App(e1, e2) => {
+                    let Value::Function(x, e) = e1.is_value()? else {
+                        return None;
+                    };
+                    let e2 = e2.is_value()?;
+                    Some(RedexInfo::App {
+                        var: x,
+                        exp: e,
+                        value: e2,
+                    })
+                }
+                Lam::Let(x, e1, e2) => {
+                    let e1 = e1.is_value()?;
+                    Some(RedexInfo::Let {
+                        var: x.clone(),
+                        value_of_var: e1,
+                        exp: e2.as_ref().clone(),
+                    })
+                }
+                Lam::Rec(x, e1, e2) => Some(RedexInfo::Rec {
+                    var: x.clone(),
+                    fix_exp: e1.as_ref().clone(),
+                    exp: e2.as_ref().clone(),
+                }),
+                Lam::Pred(e) => {
+                    let n = exp_to_num(e)?;
+                    Some(RedexInfo::Pred { n })
+                }
+                Lam::Var(_) => todo!(),
+                Lam::Lam(_, _) => todo!(),
+                Lam::Zero => todo!(),
+                Lam::Succ(_) => todo!(),
+                Lam::IfZ(_, _, _) => todo!(),
+            }
+        }
+
+        fn redex_as_exp(r: Self::RedexInfo) -> Self {
+            match r {
+                RedexInfo::App { var, exp, value } => Lam::App(
+                    Box::new(Lam::Lam(var, Box::new(exp))),
+                    Box::new(Lam::value_as_exp(value)),
+                ),
+                RedexInfo::Let {
+                    var,
+                    value_of_var,
+                    exp,
+                } => Lam::Let(
+                    var,
+                    Box::new(Lam::value_as_exp(value_of_var)),
+                    Box::new(exp),
+                ),
+                RedexInfo::Rec { var, fix_exp, exp } => {
+                    Lam::Rec(var, Box::new(fix_exp), Box::new(exp))
+                }
+                RedexInfo::Pred { n } => Lam::Pred(Box::new(num_to_exp(n))),
+            }
+        }
+
+        fn redex_step(r: Self::RedexInfo) -> Self {
+            match r {
+                RedexInfo::App { var, exp, value } => {
+                    exp.subst(var, value)
+                },
+                RedexInfo::Let { var, value_of_var, exp } => exp.subst(var, value_of_var),
+                RedexInfo::Rec { var, fix_exp, exp } => exp.subst(var, RedexInfo::Rec { var: (), fix_exp: (), exp: () }),
+                RedexInfo::Pred { n } => todo!(),
+            }
+        }
+
+        fn step(self) -> Option<Self> {
+            todo!()
+        }
+
+        fn subst(self, x: Var, t: Self) -> Self {
+            
         }
     }
 
