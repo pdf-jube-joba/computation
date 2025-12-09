183d7278aae257eb460429ec5de3b9ebd4ac5c7b
pdf-jube-joba <61172336+pdf-jube-joba@users.noreply.github.com>
2025-12-09 15:21:55 +0900

    OK

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -19,6 +19,76 @@ pub trait MealyMachine: Sized {
     fn current(&self) -> Self::This;
 }
 
+pub trait OneTime: Sized {
+    type Code: Serialize + Clone; // (A)
+    type Input: Serialize;
+    type Env: Serialize;
+
+    fn parse_code(input: &str) -> Result<Self::Code, String>;
+    fn parse_input(input: &str) -> Result<Self::Input, String>;
+    fn setup(code: Self::Code, input: Self::Input) -> Result<Self, String>;
+    fn run_onestep(&mut self);
+    fn is_terminated(&self) -> bool;
+    fn current_env(&self) -> Self::Env;
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub enum OneTimeMachine<T>
+where
+    T: OneTime,
+{
+    Machine(T),
+    Code(T::Code),
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub enum OneTimeInput<T>
+where
+    T: OneTime,
+{
+    Input(T::Input),
+    Otherwise,
+}
+
+impl<T> MealyMachine for OneTimeMachine<T>
+where
+    T: OneTime,
+{
+    type Input = T::Input;
+    type Output = bool;
+    type This = T::Env;
+    fn parse_self(input: &str) -> Result<Self, String> {
+        let code = T::parse_code(input)?;
+        Ok(OneTimeMachine::Code(code))
+    }
+    fn parse_input(input: &str) -> Result<Self::Input, String> {
+        T::parse_input(input)
+    }
+    fn step(&mut self, input: Self::Input) -> Result<Option<Self::Output>, String> {
+        match self {
+            OneTimeMachine::Code(code) => {
+                // cannot move out of code, so clone it ... (A)
+                *self = OneTimeMachine::Machine(T::setup(code.clone(), input)?);
+                Ok(None)
+            }
+            OneTimeMachine::Machine(machine) => {
+                machine.run_onestep();
+                if machine.is_terminated() {
+                    Ok(Some(true))
+                } else {
+                    Ok(Some(false))
+                }
+            }
+        }
+    }
+    fn current(&self) -> Self::This {
+        match self {
+            OneTimeMachine::Code(_) => panic!("Machine not initialized"),
+            OneTimeMachine::Machine(machine) => machine.current_env(),
+        }
+    }
+}
+
 // Object-safe wrapper used at runtime.
 pub trait WebView {
     fn step(&mut self, input: &str) -> Result<Option<JsValue>, String>;
